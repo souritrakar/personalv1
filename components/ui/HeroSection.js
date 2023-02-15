803aefe0799d5f191240e008375dabc00b5eda93
Souritra Kar <64901415+souritrakar@users.noreply.github.com>
2023-02-15 21:44:25 +0530

    Update HeroSection.js

--- components/ui/HeroSection.js
+++ components/ui/HeroSection.js
@@ -17,7 +17,7 @@ export default function HeroSection(){
             <h1 className="lg:text-7xl text-4xl z-40 font-black text-center lg:my-10 my-6 tracking-wide text-[#0F172A] dark:text-white">
               {result}
             </h1>
-            <p className="dark:text-gray-300 text-gray-500 text-center font-semibold tracking-widest lg:text-2xl z-40">Student | South City International School</p>
+            <p className="dark:text-gray-300 text-gray-500 text-center font-semibold tracking-widest lg:text-2xl z-40">Student | Programming | Web Development</p>
 
           </main>
         )
